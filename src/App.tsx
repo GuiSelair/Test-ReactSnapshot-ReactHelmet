5e80bcac7eea531ebd7e137fa6f1602b9aecec84
Guilherme Selair <guilherme.lima1997@hotmail.com>
2020-10-09 21:05:31 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <h1>
+      New Project
+      <span role="img" aria-label="rocket">
+        🚀
+      </span>
+    </h1>
+  );
+};
+
+export default App;
